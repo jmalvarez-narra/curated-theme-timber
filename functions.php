454902a2a2b6f1017e975c9cbc6730c92501e608
jmalvarez-narra <106083979+jmalvarez-narra@users.noreply.github.com>
2022-12-10 21:59:06 +0800

    Create section: header (#2)
    * add basic header
    
    * improve header styling
    
    * add dynamic header menu
    
    * fix active header menu item
    
    * add featured image to pages
    
    * improve featured image handling
    
    * add bootstrap locally
    
    * remove .DS_Store
    
    * remove js .DS_Store

--- functions.php
+++ functions.php
@@ -84,6 +84,7 @@ class StarterSite extends Timber\Site {
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
+		$context['main_navigation']  = new Timber\Menu( 'main navigation' );
 		$context['footer_menu']  = new Timber\Menu( 'footer 1' );
 		$context['site']  = $this;
 		return $context;
