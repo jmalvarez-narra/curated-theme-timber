a4d37c462ff73add3dfb3a1163024df8c39919f0
jmalvarez-narra <jm@narralabs.com>
2022-12-13 23:34:23 +0800

    improve dynamic sliders

--- template-home-page.php
+++ template-home-page.php
@@ -8,17 +8,32 @@
 $context = Timber::context();
 
 $args = array(
-// Get post type car
-'post_type' => 'cars',
-// Get all posts
-'posts_per_page' => -1,
-// Gest post by "featured" category
-// Order by post date
-'orderby' => array(
-    'date' => 'DESC'
+    // Get post type car
+    'post_type' => 'cars',
+    // Get all posts
+    'posts_per_page' => -1,
+    // Gest post by "featured" category
+    'category_name' => 'featured',
+    // Order by post date
+    'orderby' => array(
+        'date' => 'DESC',
 ));
 
-$context['cars'] = Timber::get_posts( $args );
+$context['highlights'] = Timber::get_posts( $args );
+
+$args = array(
+    // Get post type car
+    'post_type' => 'cars',
+    // Get all posts
+    'posts_per_page' => -1,
+    // Gest post by "featured" category
+    'category_name' => 'curated',
+    // Order by post date
+    'orderby' => array(
+        'date' => 'DESC',
+));
+
+$context['curated'] = Timber::get_posts( $args );
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
