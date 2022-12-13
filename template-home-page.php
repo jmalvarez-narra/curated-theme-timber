15b6bdbd63cfb32b00a325bbdb0eb188e92d6ae7
jmalvarez-narra <106083979+jmalvarez-narra@users.noreply.github.com>
2022-12-13 23:21:26 +0800

    Improve slider (#9)
    * add first look slider
    
    * improve homepage main slider

--- template-home-page.php
+++ template-home-page.php
@@ -7,6 +7,19 @@
 
 $context = Timber::context();
 
+$args = array(
+// Get post type car
+'post_type' => 'cars',
+// Get all posts
+'posts_per_page' => -1,
+// Gest post by "featured" category
+// Order by post date
+'orderby' => array(
+    'date' => 'DESC'
+));
+
+$context['cars'] = Timber::get_posts( $args );
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
