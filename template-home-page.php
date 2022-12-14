93ba873395b23630b9f7eca14f7424eff24f9789
jmalvarez-narra <jm@narralabs.com>
2022-12-14 14:00:28 +0800

    make latest multi slider dynamic

--- template-home-page.php
+++ template-home-page.php
@@ -35,6 +35,20 @@ $args = array(
 
 $context['curated'] = Timber::get_posts( $args );
 
+$args = array(
+    // Get post type car
+    'post_type' => 'cars',
+    // Get all posts
+    'posts_per_page' => -1,
+    // Gest post by "featured" category
+    'category_name' => 'latest',
+    // Order by post date
+    'orderby' => array(
+        'date' => 'DESC',
+));
+
+$context['latest'] = Timber::get_posts( $args );
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
