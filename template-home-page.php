601af18793282d852fafc809b097e6ea2b128416
jmalvarez-narra <jm@narralabs.com>
2022-12-14 23:27:36 +0800

    add placeholder image for journals multi slider

--- template-home-page.php
+++ template-home-page.php
@@ -49,6 +49,16 @@ $args = array(
 
 $context['latest'] = Timber::get_posts( $args );
 
+$args = array(
+    // Get all posts
+    'posts_per_page' => -1,
+    // Order by post date
+    'orderby' => array(
+        'date' => 'DESC',
+));
+
+$context['journal'] = Timber::get_posts( $args );
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
