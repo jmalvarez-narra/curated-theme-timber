8dc1be197cf09bc2b4bda54d0f646aa149076995
jmalvarez-narra <jm@narralabs.com>
2022-12-26 20:24:02 +0800

    fix single car post suggested carousel

--- single.php
+++ single.php
@@ -13,6 +13,20 @@ $context         = Timber::context();
 $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
 
+$args = array(
+	// Get post type car
+	'post_type' => 'cars',
+	// Get all posts
+	'posts_per_page' => -1,
+	// Gest post by "featured" category
+	'category_name' => 'latest',
+	// Order by post date
+	'orderby' => array(
+			'date' => 'DESC',
+));
+
+$context['latest'] = Timber::get_posts( $args );
+
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
