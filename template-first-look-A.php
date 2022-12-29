9881a4a9967a357672ea61b8d794cfb57e2975f9
jmalvarez-narra <jm@narralabs.com>
2022-12-29 17:13:03 +0800

    add first look template A

---
+++ template-first-look-A.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Template Name: First Look Page Template A
+ * description: >-
+  This is the first look page template
+ */
+
+
+$context = Timber::context();
+
+$args = array(
+  // Get post type car
+  'post_type' => 'cars',
+  // Get all posts
+  'posts_per_page' => -1,
+  // Gest post by "featured" category
+  'category_name' => 'curated',
+  // Order by post date
+  'orderby' => array(
+      'date' => 'DESC',
+));
+
+$context['curated'] = Timber::get_posts( $args );
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-first-look-A.twig', 'page.twig' ), $context );
