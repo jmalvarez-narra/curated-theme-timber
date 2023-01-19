e97060495b0c2cc1d6645be5d7f2ad0d211e24ca
Don Kenneth G. Vitug <ken@narralabs.com>
2023-01-20 00:22:52 +0800

    Fix Inventory Sort By

--- template-inventory.php
+++ template-inventory.php
@@ -7,11 +7,19 @@
 
 $context = Timber::context();
 
-$context['categories']= get_categories( array(
-  'name' => ['Available', 'Sold', 'Latest'],
+$categories = get_categories( array(
+  'name' => ['Inventory'],
   'hide_empty' => false,
 ));
+$inventory = $categories[0];
+$context['categories'] = get_categories(
+  array( 'parent' => $inventory->cat_ID )
+);
+$context['categories'] = array_filter($context['categories'], function ($item) {
+  return !is_numeric($item->name[0]) && strtolower($item->name) !== 'available' && strtolower($item->name) !== 'sold';
+});
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+
 Timber::render( array( 'page-inventory.twig', 'page.twig' ), $context );
